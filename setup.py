830ea987274b8b7daa6544dfc71c0a18bc912055
happystanford <wxd@stanford.edu>
2022-07-30 12:27:15 -0700

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="bipartite_graph_representation",
+    version="0.1",
+    packages=find_packages(),
+    package_dir={"": "src"},
+    description="Module for learning representation for biparitite graphs"
+)
